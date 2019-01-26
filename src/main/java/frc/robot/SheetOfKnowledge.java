ffcc5b93321a464820758e2f01d62fb63ddea5e3
Ronald <mnth.titan.robotics@gmail.com>
2019-01-26 11:33:20 -0600

    Created Sheet Of Knowledge

---
+++ src/main/java/frc/robot/SheetOfKnowledge.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public class SheetOfKnowledge {
+    public static final int MOTOR_DRIVE_FL = 0;
+    public static final int MOTOR_DRIVE_RL = 1;
+    public static final int MOTOR_DRIVE_FR = 2;
+    public static final int MOTOR_DRIVE_RR = 3;
+}
