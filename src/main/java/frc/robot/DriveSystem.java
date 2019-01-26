e84527877a545e1a3ffcf1ba2f237a43d4b0ec21
Ronald <mnth.titan.robotics@gmail.com>
2019-01-26 11:52:01 -0600

    Created drive system for all talons

--- src/main/java/frc/robot/DriveSystem.java
+++ src/main/java/frc/robot/DriveSystem.java
@@ -4,8 +4,14 @@ import edu.wpi.first.wpilibj.Talon;
 
 public class DriveSystem {
     private Talon _motorDriveFL;
+    private Talon _motorDriveRL;
+    private Talon _motorDriveFR;
+    private Talon _motorDriveRR;
 
     public DriveSystem () {
         this._motorDriveFL = new Talon(SheetOfKnowledge.MOTOR_DRIVE_FL);
+        this._motorDriveRL = new Talon(SheetOfKnowledge.MOTOR_DRIVE_RL);
+        this._motorDriveFR = new Talon(SheetOfKnowledge.MOTOR_DRIVE_FR);
+        this._motorDriveRR = new Talon(SheetOfKnowledge.MOTOR_DRIVE_RR);
     }
 }
