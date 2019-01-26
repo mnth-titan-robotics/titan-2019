29e837fceb6cedb2f8130568946eab75c9e1ee3e
Ronald <mnth.titan.robotics@gmail.com>
2019-01-26 12:16:28 -0600

    initialized and made setters for drive and turn commands

--- src/main/java/frc/robot/DriveSystem.java
+++ src/main/java/frc/robot/DriveSystem.java
@@ -8,10 +8,24 @@ public class DriveSystem {
     private Talon _motorDriveFR;
     private Talon _motorDriveRR;
 
-    public DriveSystem () {
+    private double _driveCmd;
+    private double _turnCmd;
+
+    public DriveSystem() {
         this._motorDriveFL = new Talon(SheetOfKnowledge.MOTOR_DRIVE_FL);
         this._motorDriveRL = new Talon(SheetOfKnowledge.MOTOR_DRIVE_RL);
         this._motorDriveFR = new Talon(SheetOfKnowledge.MOTOR_DRIVE_FR);
         this._motorDriveRR = new Talon(SheetOfKnowledge.MOTOR_DRIVE_RR);
+
+        this.init();
+    }
+
+    public void init() {
+        this.setCmds(0.0, 0.0);
+    }
+
+    public void setCmds(double driveCmd, double turnCmd) {
+        this._driveCmd = driveCmd;
+        this._turnCmd = turnCmd;
     }
 }
